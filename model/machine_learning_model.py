f91feec898fa9bd90f6c630587e812d3352cff6c
Abdus Salam Khazi <abduskhazi@students.uni-freiburg.de>
2021-06-16 14:16:48 +0000

    Completing the first linear regression model

--- model/machine_learning_model.py
+++ model/machine_learning_model.py
@@ -32,7 +32,8 @@ for row in input_list:
     complex_name = row_values[0][:4]
     # Feature selection. According to spearman coeffecient
     protein_indexes = sorted([17, 30, 18, 15, 16, 10, 13, 25, 47, 56, 11, 3, 31, 6, 49, 5, 9, 26, 28, 19])
-    ligand_indexes = sorted([121, 2, 120, 208, 112, 111, 182, 210, 235, 79, 90, 249, 205, 239, 233, 206, 1, 209, 212, 207])
+    # since IPC has huge values we have to use a logorithmic scale. Value 235 should be removed
+    ligand_indexes = sorted([121, 2, 120, 208, 112, 111, 182, 210, 79, 90, 249, 205, 239, 233, 206, 1, 209, 212, 207])
     row_protein = row_values[:57]
     row_ligand = row_values[57:]
     row_protein = [row_protein[i] for i in protein_indexes]
@@ -40,14 +41,31 @@ for row in input_list:
     # print(row_protein)
     # print(row_ligand)
     # exit()
-    X += [row_protein + row_ligand]
+    x_i = [row_protein + row_ligand]
+    #if 'nan' not in x_i:
+    X += x_i
     y += [regression[complex_name]]
 
-X = np.array(X)
+X = np.asarray(X, dtype='float64')
+y = np.asarray(y, dtype='float64')
 print("Len of X = ", np.shape(X))
-print("Len of y = ", len(y))
+print("Len of y = ", np.shape(y))
 
 ###################################################################################
 # MACHINE LEARNING MODEL BEGINS
 ###################################################################################
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 
+#degree = 2 # Start with 2
+#poly = PolynomialFeatures(degree, include_bias=True)
+
+#X = poly.fit_transform(X)
+#y = y
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+reg = LinearRegression().fit(X_train, y_train)
+
+print("Linear Regression score = ", reg.score(X_test, y_test))
