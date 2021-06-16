b92fc3a18e6a35f7a07128bb2b1a109370c293ed
Abdus Salam Khazi <abduskhazi@students.uni-freiburg.de>
2021-06-16 13:31:27 +0000

    Feature selection is complete before applying machine learning model

--- model/machine_learning_model.py
+++ model/machine_learning_model.py
@@ -23,17 +23,31 @@ y = []
 with open(input_var_file) as input_f:
     input_list = [r.strip() for r in input_f.readlines()]
 
+###
+# Do direct feature selection here as this will get rid of strings as well.
+###
+
 for row in input_list:
     row_values = row.split()
     complex_name = row_values[0][:4]
-    # Removing 0, 1 and 57 as they are only strings.
-    # First removing the 57th columns
-    # Then remove the first 2 columns
-    row_values = row_values[:57] + row_values[58:]
-    row_values = row_values[2:]
-    X += [row_values]
+    # Feature selection. According to spearman coeffecient
+    protein_indexes = sorted([17, 30, 18, 15, 16, 10, 13, 25, 47, 56, 11, 3, 31, 6, 49, 5, 9, 26, 28, 19])
+    ligand_indexes = sorted([121, 2, 120, 208, 112, 111, 182, 210, 235, 79, 90, 249, 205, 239, 233, 206, 1, 209, 212, 207])
+    row_protein = row_values[:57]
+    row_ligand = row_values[57:]
+    row_protein = [row_protein[i] for i in protein_indexes]
+    row_ligand = [row_ligand[i] for i in ligand_indexes]
+    # print(row_protein)
+    # print(row_ligand)
+    # exit()
+    X += [row_protein + row_ligand]
     y += [regression[complex_name]]
 
 X = np.array(X)
 print("Len of X = ", np.shape(X))
 print("Len of y = ", len(y))
+
+###################################################################################
+# MACHINE LEARNING MODEL BEGINS
+###################################################################################
+
