a173da76014d176aebbbc6c8a1dc8b31f101e1c2
Abdus Salam Khazi <abduskhazi@students.uni-freiburg.de>
2021-07-10 18:24:58 +0000

    Controlling randomness for reproducibility

--- model/random_forest_regresser.py
+++ model/random_forest_regresser.py
@@ -1,18 +1,30 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-from data_bakery import bake_train_Xy
+import matplotlib
+import matplotlib.pyplot as plt
 import random
 import numpy as np
+from data_bakery import bake_train_Xy
 
-# Obtain data for our random forest regressor
+################################################################################################################
+#                        CONTROLLING RANDOMNESS FOR REPRODUCIBILITY
+################################################################################################################
+# Note - We have seen small numerical differences e.g in the 15th decimal place. Please ignore these.
+
+seed = random.randint(0,2**32)
+print("Random seed =", seed)
+print("Please replace `seed = random.randint(0,2**32)` by `seed = %d` to reproduce this result exactly." % (seed))
+
+np.random.seed(seed)
+################################################################################################################
+
+# Obtain data for our random forest regressor.
 X, y = bake_train_Xy()
 print("X.shape =", X.shape)
 print("y.shape =", y.shape)
 
-#The reason for having a seed is to control the randomness in our models
-seed = random.randint(0,2**32)
-X_train, X_validate, y_train, y_validate = train_test_split(X, y, test_size=0.2, random_state=seed)
+X_train, X_validate, y_train, y_validate = train_test_split(X, y, test_size=0.2)
 
 regressor = RandomForestRegressor(n_estimators=100, oob_score = True, n_jobs=-1)
 regressor.fit(X_train, y_train)
@@ -21,9 +33,6 @@ y_pred = regressor.predict(X_validate)
 print("oob score = ", regressor.oob_score_, ", Validation r2 score = ", r2_score(y_validate, y_pred))
 
 #Plotting to visualize the accuracy of our model.
-import matplotlib
-import matplotlib.pyplot as plt
-
 fig = plt.figure()
 plt.plot(y_validate, y_pred, '.')
 plt.plot(range(2,14), range(2,14), '--')
@@ -34,19 +43,17 @@ plt.plot(y_train, regressor.predict(X_train), '.')
 plt.plot(range(2,14), range(2,14), '--')
 fig.savefig('temp_train.png', dpi=fig.dpi)
 
-# TO check how much importance prtein features have in our trained model -
-# We shuffle all the columns of proteins
+# TO check the importance protein features in our model -
+# We shuffle all protein feature columns one by one. Then we check the R^2 score.
+# Similar process is repeated for the ligand features.
 X_validate_backup = np.copy(X_validate)
 
-print("Shuffling protein columns")
 for i in range(55):
     np.random.shuffle(X_validate[:,i])
-print("r2 score (shuffled protein columsn) =", r2_score(y_validate, regressor.predict(X_validate)))
+print("r2 score (shuffled protein columns) =", r2_score(y_validate, regressor.predict(X_validate)))
 
 X_validate = np.copy(X_validate_backup)
-print("r2 score (backup value) =", r2_score(y_validate, regressor.predict(X_validate)))
 
-print("Shuffling ligand columns")
 for i in range(55, X.shape[1]):
     np.random.shuffle(X_validate[:,i])
 print("r2 score (shuffled ligand columns) =", r2_score(y_validate, regressor.predict(X_validate)))
